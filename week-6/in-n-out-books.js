1e1f0a8f0d511bd9994919d55f0034acdb3484d4
desertcoqui <fdetres@my365.bellevue.edu>
2021-11-27 14:50:49 -0500

    code comments WOOPSIE

--- week-6/in-n-out-books.js
+++ week-6/in-n-out-books.js
@@ -9,18 +9,26 @@
 // https://stackoverflow.com/questions/17684201/create-html-table-from-javascript-object/17684427
 // https://stackoverflow.com/questions/56111480/how-can-i-hide-a-div-until-a-button-is-clicked/56111499
 
+
+
+// assigned variables two both set of DIVs
 let divTwo = document.getElementById("card-example2");
 let divThree = document.getElementById("card-example3");
 
+//Targeting DIVs to not display on page load
 divTwo.style.display = "none";
 divThree.style.display = "none";
 
 function button() {
   let choice = document.querySelector("#favoriteList").value;
 
+  // First condition checking for "Top 3 Favorite books" to be selected
+
   if (choice == "books") {
     divTwo.style.display = "block";
     divThree.style.display ="none";
+
+    //My Fav books held in object variables w/ three name:value properties
     let ofMiceAndMen = {
       isbn: "6582356985",
       title: "Of Mice and Men",
@@ -38,8 +46,11 @@ function button() {
       pages: "102",
     };
 
+    
     document.getElementById("topFavoriteBooks").innerHTML =
       "Top 3 Favorite Books";
+
+      //Object variables binded to the html and executed if condition is true
     document.getElementById("isbn").innerHTML = "ISBN";
     document.getElementById("title").innerHTML = "Title";
     document.getElementById("pages").innerHTML = "Pages";
@@ -56,11 +67,18 @@ function button() {
     document.getElementById("childrenTitle").innerHTML = children.title;
     document.getElementById("childrenPages").innerHTML = children.pages;
 
+    //if condition is true console log will print object literal of children
+    //for testing purposes
     console.log(children);
+
+    //if first condition is false this final condition will execute if
+    //if top three favorite authors is clicked
   } else if (choice == "authors") {
     divThree.style.display = "block";
     divTwo.style.display ="none";
 
+
+    //object literals with variable of favorite author
     let taraWinston = {
       name: "Tara Winston",
       genre: "Fiction",
@@ -77,6 +95,8 @@ function button() {
 
 document.getElementById("favoriteAuthors").innerHTML = 
 "Top 3 Favorite Authors"
+
+//object literals binded to html and printed with variable.name properties
 
     document.getElementById("name").innerHTML = "Name";
     document.getElementById("genre").innerHTML = "Genre";
