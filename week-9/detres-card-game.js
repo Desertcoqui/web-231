6dad0e44a24ac9d4e8c34c7e2e7cd24aeb5d3841
desertcoqui <fdetres@my365.bellevue.edu>
2021-12-16 10:04:04 -0500

    css needs work

--- week-9/detres-card-game.js
+++ week-9/detres-card-game.js
@@ -5,31 +5,18 @@
 // Description: This week's project we will use arrays, classes, functions and switch statement
 // https://www.w3schools.com/js/js_if_else.asp
 //https://www.codegrepper.com/code-examples/javascript/javascript+alphabet+array
-
-function button() {
- getDeckOfCards();
-shuffle();
-   console.log(cards);
- newCards();
- buildPlayerCard();
-}
-
-
-  
-
 class Card {
   constructor(face, suit) {
     this.face = face;
     this.suit = suit;
   }
-}
+};
 
 class Dealer {
   constructor() {
-    //Check here if program breaks
     getDeckOfCards();
   }
-}
+};
 
 var card_count = 52;
 var cards = [];
@@ -37,48 +24,43 @@ var faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var suits = ["H", "D", "C", "S"];
 
 function getDeckOfCards() {
-  for (var counter = 0; counter < card_count; counter++) {
-    cards[counter] = new Card(
+  for (let counter = 0; counter < this.card_count; counter++) {
+    this.cards[counter] = new Card(
       this.faces[counter % 13],
       this.suits[Math.floor(counter / 13)]
     );
   }
-  return cards;
 }
-
 function shuffle() {
-  for (var firstCard = 0; firstCard < card_count; firstCard++) {
-    var tempCard = cards[firstCard];
+  for (var firstCard = 0; firstCard < this.card_count; firstCard++) {
+    var tempCard = this.cards[firstCard];
     var secondCard = Math.floor(Math.random() * card_count);
-    cards[firstCard] = cards[secondCard];
-    cards[secondCard] = tempCard;
+    this.cards[firstCard] = this.cards[secondCard];
+    this.cards[secondCard] = tempCard;
   }
-  return shuffle;
 }
-
-
 
 function buildPlayerCard(card, suitIcon, faceColor, suitColor) {
-
-
-
-  var newCard = document.getElementById("card-title");
-  newCard.innerHTML = `<div class="card player-card">
+  return `<div class="card player-card">
 <div class="card-description card-title"
-    style="color: ${faceColor} text-align:left; font-size:20px; padding-left:10px">
-    <p>What in the FUCK</p>${card.face}
+    style="color: ${faceColor}; text-align:left; font-size:20px; padding-left:10px;">
+    
+    ${card.face}
 </div>
 <div class="card-description card-content material-icons" id="shuffledCards"
     style="font-size: 28px; padding-bottom: 25px;">
-    <span class="${suitIcon}" style="color: ${suitColor}"></span>
+    <span class="${suitIcon}" style="color: ${suitColor};"></span>
 </div>`;
 }
 
 
-var cardOutputWithIcon = "";
-function newCards() {
-  for (let x of cards) {
-    switch (x.suit) {
+
+function button() {
+  let cardOutputWithIcon = "";
+  var dealer = new Dealer();
+shuffle();
+  for (let card of cards) {
+    switch (card.suit) {
       case "H":
         cardOutputWithIcon += buildPlayerCard(
           card,
@@ -111,6 +93,8 @@ function newCards() {
           "black"
         );
     }
-    console.log(x);
+
+    console.log(card);
   }
+  document.getElementById("output").innerHTML = cardOutputWithIcon;
 }
